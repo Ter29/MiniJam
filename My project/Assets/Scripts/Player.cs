6bca7ec47c2d7e3239fc0b763c9b2251be3fd378
Ter29 <smarter2906@gmail.com>
2024-11-23 17:27:11 +0100

    Joined
    With bugs

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -1,30 +1,97 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    private Rigidbody2D rb;
+    public float dashSpeed = 10f;
+    public float dashDuration = 0.2f;
+    public float dashCooldown = 3f;
+
     private Vector2 movement;
+    private Vector2 moveDirection;
+    private bool isDashing = false;
+    private float dashTime = 0f;
+    private float lastDashTime = 0f;
+
+    private float lastWPressTime = 0f;
+    private float lastAPressTime = 0f;
+    private float lastSPressTime = 0f;
+    private float lastDPressTime = 0f;
+
+    private float doubleTapTimeLimit = 0.3f;
+
+    private Rigidbody2D rb;
     public int cast;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
     void Update()
     {
-        Mooving();
-        Debug.Log(cast);
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        HandleMovementInput();
     }
-    void  FixedUpdate()
+
+    void FixedUpdate()
     {
-        rb.velocity = movement * moveSpeed;
+        if (isDashing)
+        {
+            rb.velocity = moveDirection * dashSpeed;
+            dashTime += Time.deltaTime;
+
+            if (dashTime >= dashDuration)
+            {
+                isDashing = false;
+                dashTime = 0f;
+                rb.velocity = Vector2.zero;
+            }
+        }
+        else
+        {
+            rb.velocity = movement * moveSpeed;
+        }
     }
-    private void Mooving()
+
+    void HandleMovementInput()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
-        movement = new Vector2(moveX, moveY);
+        if (Time.time - lastDashTime >= dashCooldown)
+        {
+            if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+            {
+                MovePlayer(Vector2.up, ref lastWPressTime);
+            }
+            else if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.LeftShift))
+            {
+                MovePlayer(Vector2.left, ref lastAPressTime);
+            }
+            else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.LeftShift))
+            {
+                MovePlayer(Vector2.down, ref lastSPressTime);
+            }
+            else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.LeftShift))
+            {
+                MovePlayer(Vector2.right, ref lastDPressTime);
+            }
+        }
+    }
+
+    void MovePlayer(Vector2 direction, ref float lastPressTime)
+    {
+        if (Time.time - lastPressTime <= doubleTapTimeLimit)
+        {
+            isDashing = true;
+            moveDirection = direction;
+            lastDashTime = Time.time;
+        }
+        else
+        {
+            isDashing = false;
+        }
+
+        lastPressTime = Time.time;
     }
 }
